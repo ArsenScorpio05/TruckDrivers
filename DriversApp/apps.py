b7a20acd2d40d0e1aadbd81268d6dc952aade935
Arsen Lufo <84084565+ArsenScorpio05@users.noreply.github.com>
2023-11-15 09:29:32 +0100

    Testing changes

--- DriversApp/apps.py
+++ DriversApp/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DriversappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'DriversApp'
+    
+    #adding this comment here just to test git
